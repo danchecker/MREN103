141f486958c92dd583c6ef879fe1f4f2910ba329
danchecker <35084268+danchecker@users.noreply.github.com>
2022-03-31 15:24:33 -0400

    Update Stage1_navigation.ino

--- Stage1_navigation.ino
+++ Stage1_navigation.ino
@@ -1,7 +1,7 @@
 /********************************************************
   Group42Lab4LineFollowOnOff (v1.4)
 
- 
+
   Original by H. Fernando, 25/04/2021
   Modified by B. Surgenor, 19/03/2022
 
@@ -10,46 +10,46 @@ This is a basic line following code using On/Off control on the
 differential drive robot and two line-following sensors.
 *********************************************************/
 
- 
 
-#include <Servo.h>  
+
+#include <Servo.h>
 Servo leftWheel;
 Servo rightWheel;
 
- 
+
 
 // Pin Assignments
-int RED = 10; int GRN = 9; int YLW = 5;         
+int RED = 10; int GRN = 9; int YLW = 5;
 int BUTTON = 7;         //pushbutton Pin
 int MOTOR_R = 3;       // right motor signal
 int MOTOR_L = 4;       // left motor signal
 int RSENSOR = A1; // Right Sensor on Analog Pin 1
 int LSENSOR = A2; // Left Sensor on Analog Pin 2
 
- 
 
 
-//int MOTOR_R = 3;        // right motor signal pin 
-//int MOTOR_L = 4;        // left motor signal pin 
 
- 
+//int MOTOR_R = 3;        // right motor signal pin
+//int MOTOR_L = 4;        // left motor signal pin
 
 
-// global constants
+
+
+// global constants testing github
 const int STOP_SPEED = 148;  //stop speed for motors (nominal = 150)
 const int DELTA = 12;        //DELTA is nominal speed
-const int DIFFER = 8;        //DIFFER is speed differential 
+const int DIFFER = 8;        //DIFFER is speed differential
 boolean leftLineDetected = false;
 boolean rightLineDetected = false;
-int lvalue = 0;  //left sensor value 
-int rvalue = 0;  //right sensor value 
+int lvalue = 0;  //left sensor value
+int rvalue = 0;  //right sensor value
 
- 
 
-                        
+
+
 // Set-up Routine
 void setup() {
-                
+
 // Initialize pins and wheels
   pinMode(GRN, OUTPUT); pinMode(YLW, OUTPUT); pinMode(RED, OUTPUT);
   pinMode(BUTTON, INPUT);
@@ -57,12 +57,12 @@ void setup() {
   leftWheel.attach(MOTOR_L);
   rightWheel.attach(MOTOR_R);
 
- 
+
 
 // Initialize serial and motors
   Serial.begin(9600);   // default 9600, or try 115200
 
- 
+
 
 
   runMotors(0,0);      // make sure motors stopped
@@ -71,77 +71,78 @@ void setup() {
   } while(digitalRead(BUTTON)== LOW);
 }
 
+ //this comment is simply meant to test github
  
 
 
 // Main Routine
 void loop() {
 
- 
 
-   
-      
-      //read the sensor value 
-      lvalue = analogRead(LSENSOR); 
-      rvalue = analogRead(RSENSOR); 
-      
-      //map the values into millivolts (assuming 3000 mV reference voltage) 
-      lvalue = map(lvalue,0,1023,0,3000); 
+
+
+
+      //read the sensor value
+      lvalue = analogRead(LSENSOR);
+      rvalue = analogRead(RSENSOR);
+
+      //map the values into millivolts (assuming 3000 mV reference voltage)
+      lvalue = map(lvalue,0,1023,0,3000);
       rvalue = map(rvalue,0,1023,0,3000);
 
- 
 
-      
 
- 
 
-      
+
+
+
+
   //reset LEDs
   digitalWrite(GRN, LOW); digitalWrite(RED, LOW); digitalWrite(YLW, LOW);
   leftLineDetected = isLine(LSENSOR);
   rightLineDetected = isLine(RSENSOR);
-  
+
   //Condition Red: left sensor on line, right not on line, turn left
   if(leftLineDetected && !rightLineDetected){
     turnOnLED(RED);
     runMotors(DELTA-DIFFER, DELTA); }
-    
+
   //Condition Yellow: right sensor on line, left not on line, turn right
    else if(!leftLineDetected && rightLineDetected){
     turnOnLED(YLW);
     runMotors(DELTA, DELTA-DIFFER); }
-    
+
   //Condition Green: both sensors not on line, go straight
    else if(!leftLineDetected && !rightLineDetected){
      turnOnLED(GRN);
      runMotors(DELTA, DELTA); }
 
- 
 
 
-  //Condition if both sensors are on the line 
+
+  //Condition if both sensors are on the line
    else if(leftLineDetected && rightLineDetected){
     turnOnLED(YLW);
-    delay(20); 
+    delay(20);
     runMotors(-DELTA, -DELTA);
-    delay(100); 
-    runMotors(DELTA, 0); 
+    delay(100);
+    runMotors(DELTA, 0);
     delay(250);
    }
-   
-     
-  // Condition Green/Red/Yellow: both sensors on line, stop
-  /*else{ 
-    turnOnLED(GRN);turnOnLED(RED); turnOnLED(YLW);
-    runMotors(0, 0);  
-    }    */
-}   
 
- 
+
+  // Condition Green/Red/Yellow: both sensors on line, stop
+  /*else{
+    turnOnLED(GRN);turnOnLED(RED); turnOnLED(YLW);
+    runMotors(0, 0);
+    }    */
+}
+
+
 
 //********** Functions (subroutines) ******************
 
- 
+
 
 boolean isLine(int sensor){
   int THRESHOLD = 1100;  //white/black threshold in mV
@@ -154,17 +155,17 @@ boolean isLine(int sensor){
     return false;
   }
 }
- 
+
 
 void runMotors(int delta_L, int delta_R)
 {
-  int pulse_L = (STOP_SPEED + delta_L)*10;   
+  int pulse_L = (STOP_SPEED + delta_L)*10;
   int pulse_R = (STOP_SPEED + delta_R)*10;
   leftWheel.writeMicroseconds(pulse_L);
-  rightWheel.writeMicroseconds(pulse_R); 
+  rightWheel.writeMicroseconds(pulse_R);
 }
 
- 
+
 
 // Turn on a single LED and turn others off
 void turnOnLED(int COLOUR)
@@ -175,12 +176,12 @@ void turnOnLED(int COLOUR)
   digitalWrite(COLOUR, HIGH);
 }
 
- 
+
 
 //Toggle an LED on/off
 void toggleLED(int colour){
   digitalWrite(colour, HIGH);
   delay(250);
   digitalWrite(colour, LOW);
-  delay(250); 
+  delay(250);
 }
