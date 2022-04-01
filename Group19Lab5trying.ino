b0ea6d7dbf94a7800c58e44e3f597e7bafe64b85
danchecker <35084268+danchecker@users.noreply.github.com>
2022-03-31 20:02:41 -0400

    Adding lift and curl setup to code. Lifting bucket prior to moving. Added a pause before the pivot.

--- Group19Lab5trying.ino
+++ Group19Lab5trying.ino
@@ -10,7 +10,9 @@ This code is designed to have robot follow a black line of width 1.9cm on a whit
 
 //include libraries
 #include <Servo.h>
-
+#include <Servo.h>  // Includes the library
+Servo myServoA;  // Makes a servo object to control servo A
+Servo myServoB; //Makes a servo object to control servo B
 //call upon both wheels
 Servo leftWheel;
 Servo rightWheel;
@@ -25,6 +27,16 @@ const int BUTTON = 7;        //pushbutton Pin
 int MOTOR_R = 3;        // right motor signal pin
 int MOTOR_L = 4;        // left motor signal pin
 int SHARP = A3;     // Sharp Sensor on Analog Pin 3
+int servoPinA = 11;     // Bucket servomotor #1 pin
+int servoPinB = 12; // Bucket servomotor #2 pin
+int myAngleA1 = 160; //160;    // initial angle, bucket lifts off ground if too high
+int myAngleB1 = 80; //inital angle of bucket
+int posA = myAngleA1;   // if set to 180, bucket lifts robot off of ground
+int posB = myAngleB1;
+int myAngleA2 = 120;// highest angle (lift), puts almost straight, set to 110,     still bent (i.e. not as high)
+int myAngleB2 = 115;
+int myAngleB3 = 100;
+
 
 // Delta = speed above (+) or below (-) stop speed (must be positive)
 
@@ -76,7 +88,11 @@ void setup() {
   pinMode(LSENSOR, INPUT);
   pinMode(RSENSOR, INPUT);
   pinMode(SHARP, INPUT);
-
+  // Set-up servo motors
+    myServoA.write(posA);         // Servo A starting position
+    myServoA.attach(servoPinA);   // Attaches the servo to the servo object
+    myServoB.write(posB);
+    myServoB.attach(servoPinB);
   //flash green LED
   do{
 
@@ -93,6 +109,12 @@ void setup() {
 // Main Routine
 
 void loop() {
+      delay (2000);           // A couple seconds to stand back
+      for (posA = myAngleA1; posA >= myAngleA2; posA--)
+      { // Lift action
+      myServoA.write(posA);
+      delay(20);
+      }
 
       //read the sensor value
       lvalue = analogRead(LSENSOR);
@@ -124,15 +146,16 @@ void loop() {
 
         // runMotors(-DELTA, -DELTA);
         // delay(100);
-
         runMotors(0,0);
         delay(250);
         runMotors(delta, -delta);
         delay(250);
         runMotors(delta, delta);
         delay(200);
+        runMotors(0,0);
+        delay(200);
         runMotors(-delta, -delta);
-        delay(250); 
+        delay(250);
         runMotors(delta, -delta);
       }
 
