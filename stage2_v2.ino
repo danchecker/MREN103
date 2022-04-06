371f700a9163815de45a8d8365029b1a96c5b90f
danchecker <35084268+danchecker@users.noreply.github.com>
2022-04-06 13:24:24 -0400

    longer pivot

--- stage2_v2.ino
+++ stage2_v2.ino
@@ -83,6 +83,9 @@ void loop() {
 
 if(mv_value<470){
 
+
+  runMotors(0,0);
+
   for (posA = myAngleA1; posA >= myAngleA2; posA--) { // Lift action
     myServoA.write(posA);
     delay(20);
@@ -105,7 +108,7 @@ if(mv_value<470){
     }
 
   runMotors(delta,-delta);
-  delay(2000);
+  delay(2300);
   runMotors(0,0);
 
   for (posB = myAngleB2; posB >= myAngleB1; posB--) {
