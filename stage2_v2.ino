e5f878728c80835239ae9405e10f96c7fc126db6
danchecker <35084268+danchecker@users.noreply.github.com>
2022-04-06 13:23:15 -0400

    going back to having the robot start backwards and detect the bucket

--- stage2_v2.ino
+++ stage2_v2.ino
@@ -12,147 +12,99 @@ link parallel to the ground. Lift the bucket, uncurl, recurl and then lower the 
 runMotors(delta,-delta);
 delay(1800);
 *****************************************************/
-//include libraries
-#include <Servo.h>
+#include <Servo.h>  // Includes the library
+
 Servo myServoA;  // Makes a servo object to control servo A
 Servo myServoB; //Makes a servo object to control servo B
-//call upon both wheels
 Servo leftWheel;
 Servo rightWheel;
 
-
-// Pin Assignments
-const int RED = 10;           //red LED Pin
-const int GRN = 9;           //green LED Pin
-const int YLW = 5;           //yellow LED Pin
-const int BUTTON = 7;        //pushbutton Pin
 int MOTOR_R = 3;        // right motor signal pin
 int MOTOR_L = 4;        // left motor signal pin
 int SHARP = A3;     // Sharp Sensor on Analog Pin 3
+
+const int stopPulse = 149;  // stop speed for motors (default = 150))
+const int delta =   10;       // pulse differential (default = 15)
+const float offset = 20;       // offset, slows left wheel (default = 0)
+int mv_value = 0;
+
+// Pin Assignments
+int GRN = 9;            // Green LED Pin
+int YLW = 5;            // Yellow LED Pin
+int RED = 10;           // Red LED Pin
+int BUTTON = 7;         // Pushbutton Pin
 int servoPinA = 11;     // Bucket servomotor #1 pin
 int servoPinB = 12; // Bucket servomotor #2 pin
-int myAngleA1 = 120; //160;    // initial angle, bucket lifts off ground if too high
+int myAngleA1 = 160; //160;    // initial angle, bucket lifts off ground if too high
 int myAngleB1 = 80; //inital angle of bucket
 int posA = myAngleA1;   // if set to 180, bucket lifts robot off of ground
 int posB = myAngleB1;
 int myAngleA2 = 120;// highest angle (lift), puts almost straight, set to 110,     still bent (i.e. not as high)
-int myAngleB2 = 115;
+int myAngleB2 = 170;
 int myAngleB3 = 100;
 
 
-// Delta = speed above (+) or below (-) stop speed (must be positive)
-
-const int stopPulse = 149;  // stop speed for motors (default = 150))
-const int delta =   10;       // pulse differential (default = 15)
-const float offset = 0;       // offset, slows left wheel (default = 0)
-
-
-
-const int LSENSOR = A2; // Left Sensor on Analog Pin 1
-const int RSENSOR = A1; // Right Sensor on Analog Pin 2
-
-//global variables
-
-int lvalue = 0;  //left sensor value
-int rvalue = 0;  //right sensor value
-int value = 0;
-int mv_value = 0;
-
-
-// Set-up Routine
-
+// Set-up routine
 void setup() {
 
-
-
-// Initialize led pins as outputs.
-
+// Set-up LED pins as outputs
   pinMode(GRN, OUTPUT);
   pinMode(YLW, OUTPUT);
   pinMode(RED, OUTPUT);
 
-
-
-// Initialize button pins as inputs
-
+// Set-up button pin as input
   pinMode(BUTTON, INPUT);
 
-//initialize motor control pins as servo pins
-
-  leftWheel.attach(MOTOR_L);
-  rightWheel.attach(MOTOR_R);
-
-// Initialize line following sensor pins as inputs
-
-  pinMode(LSENSOR, INPUT);
-  pinMode(RSENSOR, INPUT);
-  pinMode(SHARP, INPUT);
-  // Set-up servo motors
+// Set-up servo motors
   myServoA.write(posA);         // Servo A starting position
   myServoA.attach(servoPinA);   // Attaches the servo to the servo object
   myServoB.write(posB);
   myServoB.attach(servoPinB);
-  //flash green LED
-  do{
+//initialize motor control pins as servo pins
+  leftWheel.attach(MOTOR_L);
+  rightWheel.attach(MOTOR_R);
 
-             digitalWrite(GRN, HIGH);
-             delay(125);
-             digitalWrite(GRN, LOW);
-             delay(125);
-          }while(digitalRead(BUTTON) == LOW);
-
+  turnOnLED(GRN);
 }
 
 // Main routine
 void loop() {
-//read the sensor value
-  lvalue = analogRead(LSENSOR);
-  rvalue = analogRead(RSENSOR);
 
-  //map the values into millivolts (assuming 3000 mV reference voltage)
-  lvalue = map(lvalue,0,1023,0,4000);
-  rvalue = map(rvalue,0,1023,0,4000);
-  value = analogRead(SHARP);
-  mv_value = map(value,0,1023,0,3300); //convert AtoD count to millivolts
 
-  //
-  // runMotors(delta, delta);
-  // delay(300);
+  do {
+      toggleLED(GRN);                     // Toggle green LED on
+  } while(digitalRead(BUTTON)== LOW);     // Press button to start
 
-  // A couple seconds to stand back
+  runMotors(-delta,-delta);
+  delay(5000);
+  runMotors(0,0);
+  delay(1000);
+           // A couple seconds to stand back
 
-if(mv_value > 860){
+if(mv_value<470){
+
+  for (posA = myAngleA1; posA >= myAngleA2; posA--) { // Lift action
+    myServoA.write(posA);
+    delay(20);
+  }
+
+
+}
+
   runMotors(delta,-delta);
   delay(2000);
   runMotors(-delta,-delta);
-  delay(4000);
+  delay(4450);
   runMotors(0,0);
+  delay(500);
   delay(500);
 
   for (posB = myAngleB1; posB <= myAngleB2; posB++){
       myServoB.write(posB);
       delay(20);
     }
-  for (posA = myAngleA1; posA >= myAngleA2; posA--) { // Lift action
-    myServoA.write(posA);
-    delay(20);
-  }
-  runMotors(-delta, -delta);
-  delay(1000);
-}
 
-/*if(mv_value<470){
-  runMotors(0,0);
-
-
-  }*/
-
-
-
-
-
-
-  /*runMotors(delta,-delta);
+  runMotors(delta,-delta);
   delay(2000);
   runMotors(0,0);
 
@@ -164,8 +116,9 @@ if(mv_value > 860){
   delay(1000);
   for (posA = myAngleA2; posA <= myAngleA1; posA++) {  // Drop action
   myServoA.write(posA);
-  delay(20);*/
+  delay(20);
 
+  }
 }
 //********************* Functions (subroutines)*****************
 //Toggle an LED on/off
